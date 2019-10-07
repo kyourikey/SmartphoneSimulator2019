8eba359c51f6cec401103b53d0f9271e7e0edb5e
kyourikey <key_kun1@yahoo.co.jp>
2019-10-07 23:27:19 +0900

    Add Input manager for phone only

---
+++ Assets/Scripts/Main/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Quaternion GyroQuaternion;
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        RotationUpdate();
+    }
+
+    void RotationUpdate()
+    {
+        var gyroQ = Input.gyro.attitude;
+        //座標系そろえる
+        GyroQuaternion = new Quaternion(-gyroQ.x, -gyroQ.z, -gyroQ.y, gyroQ.w);
+    }
+}
