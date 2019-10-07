99928d8f37723b1a3db29434c285f51ef16a606d
kyourikey <key_kun1@yahoo.co.jp>
2019-10-07 23:26:41 +0900

    Add Target chaser

---
+++ Assets/Scripts/Main/TargetChaser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetChaser : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _target;
+    [SerializeField]
+    GameObject _camera;
+    [SerializeField]
+    float _chaseSpeedSmooth = 0.05f;
+
+    private Vector3 _distance;
+
+    void Start()
+    {
+        _distance = _camera.transform.position - _target.transform.position;
+    }
+
+    void Update()
+    {
+        var targetPos = _target.transform.position + _distance;
+        var nowSpeed = Vector3.zero;
+        var pos = Vector3.SmoothDamp(_camera.transform.position, targetPos, ref nowSpeed, _chaseSpeedSmooth);
+
+        _camera.transform.position = pos;
+    }
+}
